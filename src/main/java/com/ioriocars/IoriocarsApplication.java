a915070201d812773a8370243a17f3f2c77756da
Vincenzo Marcantuono <vincenzo.marcantuono@si2001.it>
2025-08-27 23:42:03 +0200

    Creazione progetto nuovo

---
+++ src/main/java/com/ioriocars/IoriocarsApplication.java
@@ -0,0 +1,13 @@
+package com.ioriocars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoriocarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoriocarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ioriocars/IoriocarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioriocars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoriocarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
