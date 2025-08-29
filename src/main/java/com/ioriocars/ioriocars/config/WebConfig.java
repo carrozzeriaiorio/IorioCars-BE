702b42ec8e65608ce7f46960f3497b147175a224
Vincenzo Marcantuono <vincenzo.marcantuono@si2001.it>
2025-08-29 20:09:35 +0200

    fix cors

--- src/main/java/com/ioriocars/ioriocars/config/WebConfig.java
+++ src/main/java/com/ioriocars/ioriocars/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
+    /*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -23,4 +23,5 @@ public class WebConfig {
             }
         };
     }
+     */
 }
